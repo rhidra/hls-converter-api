78bde7890578d9be8c34ca0f950fd34c9060eca5
Rémy Hidra <remy.hidra@gmail.com>
2021-08-25 10:16:50 +0200

    Add a basic empty CRA React app

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        Lear React
+      </header>
+    </div>
+  );
+}
+
+export default App;
