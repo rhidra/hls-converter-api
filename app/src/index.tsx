649b456ba211ae5c58a109e7b0a5e37f69c095b9
Rémy Hidra <remy.hidra@gmail.com>
2021-08-25 10:33:04 +0200

    Set the app to typescript and yarn

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
@@ -8,3 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
