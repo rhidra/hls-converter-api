0f07edb5a4653718e8e943939c759f5cddd52c5f
Rémy Hidra <remy.hidra@gmail.com>
2021-08-25 17:23:25 +0200

    Add a basic design for the app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
---
+++ app/src/pages/home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="main-layout">
+      <header>
+        <nav className="header">
+          <h1 className="title">HLS Converter</h1>
+
+          <a className="call-to-action">Access the API</a>
+        </nav>
+      </header>
+
+      <section className="dropdown">
+        
+      </section>
+    </div>
+  );
+}
