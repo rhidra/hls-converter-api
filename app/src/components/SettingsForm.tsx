efe53f05ea474e02707f51a1790dacda1ea9fa36
Rémy Hidra <remy.hidra@gmail.com>
2021-08-26 17:55:56 +0200

    Add a more complex form

--- app/src/components/SettingsForm.tsx
+++ app/src/components/SettingsForm.tsx
@@ -3,6 +3,8 @@ import Select from "./Select";
 
 const SettingsForm: FC = () => {
   const [encodingSpeed, setEncodingSpeed] = useState('medium');
+  const [segmentSize, setSegmentSize] = useState('6');
+  const [framerate, setFramerate] = useState('25');
 
   return (
     <>
@@ -16,6 +18,33 @@ const SettingsForm: FC = () => {
           {value: 'slow', label: 'Slow'},
           {value: 'medium', label: 'Medium'},
           {value: 'fast', label: 'Fast'},
+        ]}
+      />
+      <Select
+        id="segment-size"
+        label="Segments size"
+        value={segmentSize}
+        onChange={v => setSegmentSize(v)}
+        options={[
+          {value: '2', label: '2'},
+          {value: '4', label: '4'},
+          {value: '6', label: '6 (recommanded)'},
+          {value: '8', label: '8'},
+          {value: '10', label: '10'},
+        ]}
+      />
+      <Select
+        id="framerate"
+        label="Framerate"
+        value={framerate}
+        onChange={v => setFramerate(v)}
+        options={[
+          {value: '23', label: '23.976'},
+          {value: '24', label: '24'},
+          {value: '25', label: '25'},
+          {value: '30', label: '30'},
+          {value: '60', label: '60'},
+          {value: '120', label: '120'},
         ]}
       />
     </form>
