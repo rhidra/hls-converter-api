cd2d8fdb14945d9ce9fe2af71125cf6d5ebf17f9
Rémy Hidra <remy.hidra@gmail.com>
2021-09-20 16:46:03 +0200

    Fix footer links

--- app/src/layouts/Footer.tsx
+++ app/src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa';
 const Footer: FC = () => {
   return (
     <footer>
-      <a href="/" target="_blank" className="github">
+      <a href="https://github.com/rhidra/hls-converter-api" target="_blank" className="github">
         <FaGithub/>&nbsp;
         Github
       </a>
@@ -14,7 +14,7 @@ const Footer: FC = () => {
         Rémy Hidra
       </div>
 
-      <a href="/" target="_blank" className="portfolio">
+      <a href="https://rhidra.github.io/" target="_blank" className="portfolio">
         <img src="https://img.icons8.com/ios-glyphs/30/000000/fire-element--v1.png" width="24px" alt="Flame icon"/>&nbsp;
         More projects
       </a>
